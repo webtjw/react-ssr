b6c65808bf1864b3f169b53cdca96d00f6b7af02
jiawei <webtjw@foxmail.com>
2018-06-07 22:34:23 +0800

    url fix

--- request/axios.js
+++ request/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosRequest = axios.create({
-  baseURL: (process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:3001' : '') + '/sapi',
+  baseURL: '/sapi',
   withCredentials: true
 })
 
@@ -9,6 +9,7 @@ axiosRequest.interceptors.response.use(response => {
   if (response && response.data) return response.data
   else return null
 }, error => {
+  console.log(error)
   return Promise.resolve(error) // component AsyncTips has to receive result even if the request fails.
 })
 
