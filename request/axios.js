31ed2c2457f75803d2cdabaa8e12d4ded74b6e06
jiawei <webtjw@foxmail.com>
2018-06-07 11:05:54 +0800

    modify

--- request/axios.js
+++ request/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosRequest = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:3001' : '/',
+  baseURL: (process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:3001' : '') + '/sapi',
   withCredentials: true
 })
 
