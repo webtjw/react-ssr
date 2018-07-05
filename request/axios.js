a1dcb43f4808dcba717fafbf94cb2de9d4454891
tanjiawei <jiawei.tan@doverfs.com>
2018-07-05 11:34:40 +0800

    change ajax

--- request/axios.js
+++ request/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosRequest = axios.create({
-  baseURL: '/sapi',
+  baseURL: '/blogApi',
   withCredentials: true
 })
 
@@ -9,7 +9,6 @@ axiosRequest.interceptors.response.use(response => {
   if (response && response.data) return response.data
   else return null
 }, error => {
-  console.log(error)
   return Promise.resolve(error) // component AsyncTips has to receive result even if the request fails.
 })
 
